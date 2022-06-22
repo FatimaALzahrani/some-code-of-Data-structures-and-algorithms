20c427e42811f61e13f30856fa3dc0c543de00f2
FatimaALzahrani <107775566+FatimaALzahrani@users.noreply.github.com>
2022-06-22 22:20:23 +0300

    Node of Sngle Linked List class
    this is the node of Sngle Linked List class and must be in the same packeg of Sngle Linked List class

---
+++ Node.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fatimah mohammed
+ */
+public class Node {
+        int data; 
+	 Node next; 
+// Constructor to create a new node 
+// Next is by default initialized  as null 
+	Node(int d){
+        data = d; next=null;
+        }
+}
