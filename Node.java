fccaf1914c90c28c228c01cfe86e96cf1d0be798
FatimaALzahrani <107775566+FatimaALzahrani@users.noreply.github.com>
2022-09-03 12:51:11 +0300

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Stack_Using_List;
+
+/**
+ *
+ * @author 12fat
+ */
+public class Node {
+	int data;
+	Node next;   // by default it refers to null
+    Node (int d) {data = d;  }   //constructor of the Node class
+}
